accb0ea8770b28df7798625fca1e910803feea38
tiida26 <ambo.sun@gmail.com>
2014-11-13 00:58:39 +0900

    第４回 扱えるデータ型を見てみる

--- php/index.php
+++ php/index.php
@@ -2,10 +2,28 @@
 
 // 変数 : 値につけるラベル
 
-// 英数字_が使える
-// $のあとの数字はだめ
-// 大文字小文字は区別される apple Apple
+// 文字列 "" '' 
+// 数値(整数、少数)
+// 論理値(true/false)
+// null(何もない)
+// 配列
+// オブジェクト
 
-$message = "Hello World!!";
+$message = "Hello World!";
+$x = 5;
+$y = 1.22;
+$flag = true;
+$n = null;
 
-echo $message;
+var_dump($message);
+var_dump($x);
+var_dump($y);
+var_dump($flag);
+var_dump($n);
+
+// ↑をブラウザから見ると 'string(12) "Hello World!" int(5) float(1.22) bool(true) NULL' という感じで出てくる
+// string : 文字列
+// int : 整数値
+// float : 小数点
+// bool : ブーリアン？ 真偽値
+// NULL : null
