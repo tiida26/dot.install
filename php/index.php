9ae10dc4a011f24c1d78a182e9dd220c1be594cf
tiida26 <ambo.sun@gmail.com>
2014-11-16 11:17:07 +0900

    第５回 定数を使ってみる

--- php/index.php
+++ php/index.php
@@ -1,29 +1,13 @@
 <?php
 
-// 変数 : 値につけるラベル
+// 変数 : データにつけるラベル(変更可能)
 
-// 文字列 "" '' 
-// 数値(整数、少数)
-// 論理値(true/false)
-// null(何もない)
-// 配列
-// オブジェクト
+// 定数 : 変更不可
 
-$message = "Hello World!";
-$x = 5;
-$y = 1.22;
-$flag = true;
-$n = null;
+define("ADMIN_EMAIL", "ambo.sun@gmail.com");    // 通例 で 定数は大文字で書くことが多い
+define("LIST_COUNT", 15);
 
-var_dump($message);
-var_dump($x);
-var_dump($y);
-var_dump($flag);
-var_dump($n);
+$admin_email = "ambo.sun@gmail.com";
 
-// ↑をブラウザから見ると 'string(12) "Hello World!" int(5) float(1.22) bool(true) NULL' という感じで出てくる
-// string : 文字列
-// int : 整数値
-// float : 小数点
-// bool : ブーリアン？ 真偽値
-// NULL : null
+echo ADMIN_EMAIL;    // 定数を展開するときは $ が要らないっぽい。。。。
+echo $admin_email;
