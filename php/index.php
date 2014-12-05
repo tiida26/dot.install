9af217197fcc24538b7f298b9e157b29e0074666
tiida26 <ambo.sun@gmail.com>
2014-12-05 23:35:15 +0900

    第１７回 配列をループさせる

--- php/index.php
+++ php/index.php
@@ -1,9 +1,21 @@
 <?php
 
-// 連想配列
+// 配列に関するループ
 
+
+$youbi = array("Sun", "Mon", "Tue");
 $sales = array("tanaka"=>150, "taguchi"=>200);
 
-echo $sales["tanaka"];
 
-// 普通の配列同様にunsetなどもできる
+// foreach
+/*
+foreach ($youbi as $y) {
+	echo $y;
+}
+*/
+
+
+foreach ($sales as $name => $amount) {
+	echo $name, $amount;
+}
+
