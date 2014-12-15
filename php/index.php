902684117be5b6d13a7ad937fc9748e5f1ee3492
tiida26 <ambo.sun@gmail.com>
2014-12-16 01:16:15 +0900

    第２５回 日付/時間関数を使ってみよう

--- php/index.php
+++ php/index.php
@@ -1,23 +1,12 @@
 <?php
 
-// 配列に関する関数
+// 日付や時間に関する関数
 
-$members = array("tanaka", "sasaki", "kimura", "yoshida", "uchida");
+// Unixタイムスタンプ（1970/1/1 00:00:00からの経過秒数）
 
-// count（要素の数を返す）
-echo count($members);
-
-// sort（要素をソートする）
-sort($members);
-var_dump($members);
-
-// in_array（配列の中にそれが入っているかどうか）
-if (in_array("tanaka", $members)){
-	echo "BINGO!";
-}
-
-// implode（区切り文字を置き換える）/explode（任意の区切り文字で分解する）
-$atstr = implode("@", $members);
-var_dump(explode("@", $atstr));
-
+var_dump(time());    //現在の時間をUnixタイムで出す
+var_dump(mktime(7,55,0,6,7,2012));    // 2012/6/7 7:55:00 をUnixタイムにする
+var_dump(strtotime("2012/6/7 7:55:00"));    // 人が読めるっぽいやつをUnixタイムにする
+var_dump(strtotime("last Sunday"));    // 人が読めるっぽいやつをUnixタイムにする
+var_dump(strtotime("+ 2day"));    // 人が読めるっぽいやつをUnixタイムにする
 
