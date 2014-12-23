b79591a98ee0ddbea2f8b19d93fbaad39bcc46b7
tiida26 <ambo.sun@gmail.com>
2014-12-24 00:58:01 +0900

    第２８回 ファイルにデータを書き込む２

--- php/index.php
+++ php/index.php
@@ -8,8 +8,14 @@ $contents = "こんにちは！";
 if (is_writable($testFile)) {
 
     // ファイルをオープンできたか？
+    // $fp = fopen($testFile, "a");    // a はファイルの最後に書き込む。 w はファイルの最初から書き込む。最初から書き込むと上書きされてしまうらしい。
+    if (!$fp = fopen($testFile, "a")) {    // fopen関数はうまくいったらファイルポインタリソースというのが返るらしい。そうでなければFALSEが返る。通常はこの返り値を変数に入れて処理をさせるらしい。
+        echo "could not open!";
+        exit
+    }
 
     // 書き込めたか？
+    fwrite($fp, $contents);
 
     // 終了処理
 
