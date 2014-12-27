9eb9ba8a8aab05be2185dacbed8415ca032aed62
tiida26 <ambo.sun@gmail.com>
2014-12-27 17:40:33 +0900

    第３０回ファイルからデータを読み出そう

--- php/index.php
+++ php/index.php
@@ -3,35 +3,17 @@
 // ファイルの読み書き
 
 $testFile = "test.dat";
-$contents = "こんにちは！";
 
-var_dump(is_writable($testFile));
 
-if (is_writable($testFile)) {
-
-    // ファイルをオープンできたか？
-    // $fp = fopen($testFile, "a");    // a はファイルの最後に書き込む。 w はファイルの最初から書き込む。最初から書き込むと上書きされてしまうらしい。
-    if (!$fp = fopen($testFile, "a")) {    // fopen関数はうまくいったらファイルポインタリソースというのが返るらしい。そうでなければFALSEが返る。通常はこの返り値を変数に入れて処理をさせるらしい。
-        echo "could not open!";
-        exit;
-    }
-
-    // 書き込めたか？
-    if (fwrite($fp, $contents) === false) {
-        echo "could not write!";
-        exit;
-    }
-
-    echo "success!";
-
-    // 終了処理
-    fclose($fp);
-
-} else {
-    echo "not writable";
+if (!$fp = fopen($testFile, "r")) {   // rwite の r
+    echo "could not open";
     exit;
 }
 
-// is_writable: ファイルが書き込みできるかを true or false で返す関数
-// exit: プログラムを終了させる命令
+$contents = fread($fp, filesize($testFile));    // filesize($testFile) でファイルの最大サイズまで読み込む
+
+var_dump($contents);
+
+fclose($fp);
+
 
