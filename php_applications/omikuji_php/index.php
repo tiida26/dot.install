1a06aba74e8b02f75c93fd1bbbaa38606d7ef4f3
tiida26 <ambo.sun@gmail.com>
2015-01-03 17:15:29 +0900

    第１回 開発の準備を整える

---
+++ php_applications/omikuji_php/index.php
@@ -0,0 +1,17 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charaset="utf-8">
+    <title>おみくじ</title>
+</head>
+<body>
+    <h1>おみくじ</h1>
+    <p>今日の運勢は「」です！</p>
+    <p><a href="">もう一度！</a></p>
+</body>
+</html>
