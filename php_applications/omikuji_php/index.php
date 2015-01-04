82bf00c48cec096eb841135d8bf1e1a28d7f3feb
tiida26 <ambo.sun@gmail.com>
2015-01-04 23:50:54 +0900

    第2回 おみくじを作ってみる。

--- php_applications/omikuji_php/index.php
+++ php_applications/omikuji_php/index.php
@@ -1,5 +1,14 @@
 <?php
 
+$omikuji = array('小吉', '大吉', '凶', '末吉', 'もう一度');
+
+$result = $omikuji[mt_rand(0, count($omikuji)-1)];
+
+/*
+mt_rand: 指定の範囲の数からランダムに数次をとる
+count: 配列の個数をとる
+
+*/
 
 ?>
 
@@ -11,7 +20,7 @@
 </head>
 <body>
     <h1>おみくじ</h1>
-    <p>今日の運勢は「」です！</p>
-    <p><a href="">もう一度！</a></p>
+    <p>今日の運勢は「<?php echo $result; ?>」です！</p>
+    <p><a href="<?php echo $_SERVER["SCRIPT_NAME"] ?>">もう一度！</a></p>
 </body>
 </html>
